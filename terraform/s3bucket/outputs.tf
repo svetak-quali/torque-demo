b3dbc694d095af4cbc6a740ba09dff70ddd9ba66
svetak-quali <58253836+svetak-quali@users.noreply.github.com>
2022-08-21 10:36:47 +0300

    Add files via upload

---
+++ terraform/s3bucket/outputs.tf
@@ -0,0 +1,5 @@
+
+output "ssm_value" {
+  value = aws_s3_bucket.bucket.bucket
+}
+
---
+++ terraform/s3bucket/variables.tf
@@ -0,0 +1,11 @@
+
+variable "aws_region" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "name" {
+  type    = string
+  default = "test_name"
+}
+
