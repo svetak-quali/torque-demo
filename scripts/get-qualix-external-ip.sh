477a7a36cca3cc50f56ce2bc86fe8a3024b6c330
svetak-quali <58253836+svetak-quali@users.noreply.github.com>
2022-09-21 18:47:48 +0300

    Update get-qualix-external-ip.sh

--- scripts/get-qualix-external-ip.sh
+++ scripts/get-qualix-external-ip.sh
@@ -4,7 +4,7 @@ export qualix_ip=$(kubectl get service guacamole -n svetak -o jsonpath="{.status
 export qualix_hostname=$(kubectl get service guacamole -n svetak -o jsonpath="{.status.loadBalancer.ingress[0].hostname}")
 #export POD_NAME=$(kubectl get pods -n svetak | grep guacamole | awk '{print $1}')
 export POD_NAME=$(kubectl get pods -n svetak --sort-by=.metadata.creationTimestamp --no-headers | grep guacamole | tac | awk 'NR==1{print $1}')
-export qualix_outbound_ip=$(kubectl exec -i -t -n svetak $POD_NAME -c guacamole -- sh -c "curl icanhazip.com")
+export qualix_outbound_ip=$(kubectl exec -i -n svetak $POD_NAME -c guacamole -- sh -c "curl icanhazip.com")
 kubectl exec -i -n svetak $POD_NAME -c guacamole -- sh -c "touch /disableValidateLink"
 echo $qualix_ip
 echo $qualix_hostname
